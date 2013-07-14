94837edbb7a8090ab60323dd4d9f82eb567ed799
Vasily Zubov <bla@yahoo.co.uk>
2013-07-14 21:49:00 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var text = new Buffer(100);
+
+text = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(text.toString());
 });
 
 var port = process.env.PORT || 5000;
