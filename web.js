9e04c0339b904c2b5d95a52e079d587efea7c6de
Vasily Zubov <bla@yahoo.co.uk>
2013-07-14 22:20:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,15 +2,17 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var text = new Buffer(100);
+//var text = new Buffer(100);
 
-text.write(fs.readFileSync('index.html'));
+//text.write(fs.readFileSync('./index.html'));
 
-var chartext = text.toString();
+//var chartext = text.toString();
 
 app.get('/', function(request, response) {
-  response.send("New");
-port = process.env.PORT || 5000;
+  response.send((fs.readFileSync("./index.html")).toString());
+});
+
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
